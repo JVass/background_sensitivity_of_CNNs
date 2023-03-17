eabf7ecb508b81453955059d33abd53b1eed752d
JVass <johnvslk@gmail.com>
2023-03-17 20:03:10 +0000

    Changed to Autoencoder. Removed the 'custom' upsampling convolution to Transposed Conv 2D.

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ import torch
 from global_vars import *
 
 class Autoencoder(nn.Module):
-    def __init__(self, n_channels = 1, number_of_filters = 128):
+    def __init__(self, n_channels = 1, number_of_filters = 128, device_name = "cpu"):
         super(Autoencoder, self).__init__()
         self.n_channels = n_channels
 
@@ -21,19 +21,19 @@ class Autoencoder(nn.Module):
             self.window_tensor = torch.ones_like(torch.blackman_window(N_FFT))
 
         self.max_pool = nn.MaxPool2d(kernel_size=2)
-        self.relu = nn.ReLU()
+        self.tanh = nn.Tanh()
 
         self.featurizer = nn.Conv2d(1, nof, kernel_size = 3, padding = "same")
-        self.down1 = nn.Conv2d(nof, nof//2, kernel_size = 3, padding = "same")
-        self.down2 = nn.Conv2d(nof//2, nof//(2**2), kernel_size = 3, padding = "same")
-        self.down3 = nn.Conv2d(nof//(2**2), nof//(2**3), kernel_size = 3, padding = "same")
+        self.down1 = nn.Conv2d(nof, 16, kernel_size = 3, padding = "same")
+        self.down2 = nn.Conv2d(16, 8, kernel_size = 3, padding = "same")
+        # self.down3 = nn.Conv2d(8, 4, kernel_size = 3, padding = "same")
 
         self.upsample = nn.Upsample(scale_factor = 2, mode = "bilinear", align_corners=True)
 
-        self.up1 = nn.Conv2d(nof//(2**3), nof//(2**2), kernel_size = 3, padding = "same")
-        self.up2 = nn.Conv2d(nof//(2**2), nof//(2**1), kernel_size = 3, padding = "same")
-        self.up3 = nn.Conv2d(nof//(2**1), nof, kernel_size = 3, padding = "same")
-        self.output_layer = nn.Conv2d(nof, 1, kernel_size = 1, padding = "same")
+        # self.up1 = nn.ConvTranspose2d(4, 8, kernel_size = 3, stride = 2, output_padding = 1, padding = 1)
+        self.up2 = nn.ConvTranspose2d(8, 16, kernel_size = 3, stride = 2, output_padding = 1, padding = 1)
+        self.up3 = nn.ConvTranspose2d(16, 32, kernel_size = 3, stride = 2, output_padding = 1, padding = 1)
+        self.output_layer = nn.Conv2d(32, 1, kernel_size = 1, padding = "same")
 
     def stft(self, audio):
         return torch.stft(audio, n_fft = N_FFT, hop_length = NOVERLAP, window = self.window_tensor, return_complex = True)
@@ -62,34 +62,29 @@ class Autoencoder(nn.Module):
         y = self.down1(y)
         y = self.max_pool(y)
 
-        y = self.down2(y)
-        y = self.max_pool(y)
+        # Bottleneck
+        y_bottleneck = self.down2(y)
 
-        # Bottleneck (compressed representation)
-        y_bottleneck = self.down3(y)
-
-        # Decoder
-        y = self.upsample(y_bottleneck)
-        y = self.up1(y)
-
-        y = self.upsample(y)
-        y = self.up2(y)
+        y = self.up2(y_bottleneck)
 
         y = self.up3(y)
-        y = self.upsample(y)
 
         # output spectrogram
         output_magnitude = self.output_layer(y)
 
-        # audio reconstruction
-        predicted_spectrogram = torch.cat((output_magnitude, torch.zeros(output_magnitude.shape[0], output_magnitude.shape[1], 1, output_magnitude.shape[-1])),
+        # audio reconstruction in time domain
+        predicted_spectrogram = torch.cat((output_magnitude, 
+                                           torch.zeros(size = (output_magnitude.shape[0], output_magnitude.shape[1], 1, output_magnitude.shape[-1])).to(self.device)),
                                     dim = 2)
+        
         predicted_spectrogram = predicted_spectrogram * torch.exp(1j * phase)
 
         predicted_spectrogram = torch.squeeze(predicted_spectrogram)
 
-        output = torch.zeros_like(x)
-
         output = torch.istft(predicted_spectrogram, n_fft = N_FFT, hop_length = NOVERLAP, window = self.window_tensor)
 
         return output, y_bottleneck
+    
+    def set_device(self, device):
+        self.window_tensor = self.window_tensor.to(device)
+        self.device = device
