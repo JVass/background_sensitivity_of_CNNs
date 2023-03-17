51b06e4b6791f212a4a1c38d372362c67f3e666f
JVass <johnvslk@gmail.com>
2023-03-17 20:01:50 +0000

    Added learning, early stopping, loss function weighting parameters.

--- global_vars.py
+++ global_vars.py
@@ -2,10 +2,25 @@ import torch
 
 PATH_TO_DATASET = ""
 
-SR = 22050
+SR = 48000
 
+# STFT parameters
 N_FFT = 512
 NOVERLAP = 256
 WINDOW = "hamming"
 
-CHUNK_SIZE = NOVERLAP*670
+# Chunk sizes
+CHUNK_SIZE = NOVERLAP*254
+
+# Learning parameters
+LEARNING_RATE = 1e-3
+BATCH_SIZE = 64
+EPOCHS = 100
+
+# Early stopping parameters
+PATIENCE = 5
+MIN_DELTA = 0.02
+
+# Loss function weights
+TIME_WEIGHT = 10
+FREQ_WEIGHT = 0.1
